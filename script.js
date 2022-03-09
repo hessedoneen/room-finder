73ba777d5730e050caad1d47fa764a4ca505a0ab
Alexander Michael House <housea@umich.edu>
2022-03-09 18:47:10 -0500

    map overlay

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 // Listen for submit, get room_num and store in localStorage
 document.getElementById('signup').addEventListener('submit', () => {
-    const room_num = document.getElementById("mySearch").value;
-    localStorage.setItem("room_num", room_num);
+  const room_num = document.getElementById("mySearch").value;
+  console.log(room_num);
+  localStorage.setItem("room_num", room_num);
 });
 
 
@@ -12,25 +13,25 @@ function readCSV(room_num, getCoor) {
     var xhr = new XMLHttpRequest();
     xhr.open("GET", url);
     xhr.responseType = "blob";
-    xhr.addEventListener('load', function() {
-        convertBlob(xhr.response);
+    xhr.addEventListener('load', function () {
+      convertBlob(xhr.response);
     });
     xhr.send();
   };
 
   var blobToFile = function (blob, name) {
-      blob.lastModifiedDate = new Date();
-      blob.name = name;
-      return blob;
+    blob.lastModifiedDate = new Date();
+    blob.name = name;
+    return blob;
   };
 
-  var GetFileObjectFromURL = function(filePathOrUrl, convertBlob) {
+  var GetFileObjectFromURL = function (filePathOrUrl, convertBlob) {
     GetFileBlobUsingURL(filePathOrUrl, function (blob) {
-        convertBlob(blobToFile(blob, 'roominfo.csv'));
+      convertBlob(blobToFile(blob, 'roominfo.csv'));
     });
   };
 
-  var FileURL="roominfo.csv"
+  var FileURL = "roominfo.csv"
   GetFileObjectFromURL(FileURL, function (fileObject) {
     console.log(fileObject);
     // Read the fileObject
@@ -104,86 +105,179 @@ function initMap() {
   // Init map with location 
   const latitude = parseFloat(found_lat);
   const longitude = parseFloat(found_long);
+  console.log(latitude);
+  console.log(longitude);
 
   const new_loc = { lat: latitude, lng: longitude };
   // The map, centered at given location
   const map = new google.maps.Map(document.getElementById("map"), {
     zoom: 20,
     center: new_loc,
+    mapId: '1d764dc13899b61e'
   });
+  console.log(map.center);
   // The marker, positioned at GG Brown
   const marker = new google.maps.Marker({
     position: new_loc,
     map: map,
   });
+
+  const bounds = new google.maps.LatLngBounds(
+    new google.maps.LatLng(42.29279758926315, -83.71516984441227),
+    new google.maps.LatLng(42.29405892225341, -83.71278616217957)
+  );
+
+  /* IMAGE OVERLAY */
+  let image = 'GGB_2nd_floor_pngfile_squish2.png';
+
+  /**
+   * The custom USGSOverlay object contains the USGS image,
+   * the bounds of the image, and a reference to the map.
+   */
+  class USGSOverlay extends google.maps.OverlayView {
+    bounds;
+    image;
+    div;
+    constructor(bounds, image) {
+      super();
+      this.bounds = bounds;
+      this.image = image;
+    }
+
+    /**
+       * onAdd is called when the map's panes are ready and the overlay has been
+       * added to the map.
+       */
+    onAdd() {
+      this.div = document.createElement("div");
+      this.div.style.borderStyle = "none";
+      this.div.style.borderWidth = "0px";
+      this.div.style.position = "absolute";
+
+      // Create the img element and attach it to the div.
+      const img = document.createElement("img");
+
+      img.src = this.image;
+      img.style.width = "100%";
+      img.style.height = "100%";
+      img.style.position = "absolute";
+      this.div.appendChild(img);
+
+      // Add the element to the "overlayLayer" pane.
+      const panes = this.getPanes();
+
+      panes.overlayLayer.appendChild(this.div);
+    }
+    draw() {
+      // We use the south-west and north-east
+      // coordinates of the overlay to peg it to the correct position and size.
+      // To do this, we need to retrieve the projection from the overlay.
+      const overlayProjection = this.getProjection();
+      // Retrieve the south-west and north-east coordinates of this overlay
+      // in LatLngs and convert them to pixel coordinates.
+      // We'll use these coordinates to resize the div.
+      const sw = overlayProjection.fromLatLngToDivPixel(
+        this.bounds.getSouthWest()
+      );
+      const ne = overlayProjection.fromLatLngToDivPixel(
+        this.bounds.getNorthEast()
+      );
+
+      // Resize the image's div to fit the indicated dimensions.
+      if (this.div) {
+        this.div.style.left = sw.x + "px";
+        this.div.style.top = ne.y + "px";
+        this.div.style.width = ne.x - sw.x + "px";
+        this.div.style.height = sw.y - ne.y + "px";
+      }
+    }
+  }
+  const overlay = new USGSOverlay(bounds, image);
+
+  overlay.setMap(map);
+
+  const toggleButton = document.createElement("button");
+
+
+  toggleButton.textContent = "Search";
+  toggleButton.classList.add("custom-map-control-button");
+
+
+  toggleButton.addEventListener("click", () => {
+    localStorage.clear();
+    window.location.replace('./index.html');
+  });
+
+  map.controls[google.maps.ControlPosition.TOP_RIGHT].push(toggleButton);
 }
+
 
 
 /// -----------TODO---------------------------------------------------------------------------
 
-/* OVERLAY MAP */
-/**
-   * The custom USGSOverlay object contains the USGS image,
-   * the bounds of the image, and a reference to the map.
-   */
- class USGSOverlay extends google.maps.OverlayView {
-  bounds;
-  image;
-  div;
-  constructor(bounds, image) {
-    super();
-    this.bounds = bounds;
-    this.image = image;
-  }
-  /**
-   * onAdd is called when the map's panes are ready and the overlay has been
-   * added to the map.
-   */
-  onAdd() {
-    this.div = document.createElement("div");
-    this.div.style.borderStyle = "none";
-    this.div.style.borderWidth = "0px";
-    this.div.style.position = "absolute";
+// /* OVERLAY MAP */
+// /**
+//    * The custom USGSOverlay object contains the USGS image,
+//    * the bounds of the image, and a reference to the map.
+//    */
+//  class USGSOverlay extends google.maps.OverlayView {
+//   bounds;
+//   image;
+//   div;
+//   constructor(bounds, image) {
+//     super();
+//     this.bounds = bounds;
+//     this.image = image;
+//   }
+//   /**
+//    * onAdd is called when the map's panes are ready and the overlay has been
+//    * added to the map.
+//    */
+//   onAdd() {
+//     this.div = document.createElement("div");
+//     this.div.style.borderStyle = "none";
+//     this.div.style.borderWidth = "0px";
+//     this.div.style.position = "absolute";
 
-    // Create the img element and attach it to the div.
-    const img = document.createElement("img");
+//     // Create the img element and attach it to the div.
+//     const img = document.createElement("img");
 
-    img.src = this.image;
-    img.style.width = "100%";
-    img.style.height = "100%";
-    img.style.position = "absolute";
-    this.div.appendChild(img);
+//     img.src = this.image;
+//     img.style.width = "100%";
+//     img.style.height = "100%";
+//     img.style.position = "absolute";
+//     this.div.appendChild(img);
 
-    // Add the element to the "overlayLayer" pane.
-    const panes = this.getPanes();
+//     // Add the element to the "overlayLayer" pane.
+//     const panes = this.getPanes();
 
-    panes.overlayLayer.appendChild(this.div);
-  }
-  draw() {
-    // We use the south-west and north-east
-    // coordinates of the overlay to peg it to the correct position and size.
-    // To do this, we need to retrieve the projection from the overlay.
-    const overlayProjection = this.getProjection();
-    // Retrieve the south-west and north-east coordinates of this overlay
-    // in LatLngs and convert them to pixel coordinates.
-    // We'll use these coordinates to resize the div.
-    const sw = overlayProjection.fromLatLngToDivPixel(
-      this.bounds.getSouthWest()
-    );
-    const ne = overlayProjection.fromLatLngToDivPixel(
-      this.bounds.getNorthEast()
-    );
+//     panes.overlayLayer.appendChild(this.div);
+//   }
+//   draw() {
+//     // We use the south-west and north-east
+//     // coordinates of the overlay to peg it to the correct position and size.
+//     // To do this, we need to retrieve the projection from the overlay.
+//     const overlayProjection = this.getProjection();
+//     // Retrieve the south-west and north-east coordinates of this overlay
+//     // in LatLngs and convert them to pixel coordinates.
+//     // We'll use these coordinates to resize the div.
+//     const sw = overlayProjection.fromLatLngToDivPixel(
+//       this.bounds.getSouthWest()
+//     );
+//     const ne = overlayProjection.fromLatLngToDivPixel(
+//       this.bounds.getNorthEast()
+//     );
 
-    // Resize the image's div to fit the indicated dimensions.
-    if (this.div) {
-      this.div.style.left = sw.x + "px";
-      this.div.style.top = ne.y + "px";
-      this.div.style.width = ne.x - sw.x + "px";
-      this.div.style.height = sw.y - ne.y + "px";
-    }
-  }
+//     // Resize the image's div to fit the indicated dimensions.
+//     if (this.div) {
+//       this.div.style.left = sw.x + "px";
+//       this.div.style.top = ne.y + "px";
+//       this.div.style.width = ne.x - sw.x + "px";
+//       this.div.style.height = sw.y - ne.y + "px";
+//     }
+//   }
 
-}
+// }
 
 
 // Do a new search from the map page
